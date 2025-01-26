35925113ce1f28d54f559d90caa92faf19a44dd3
Lucccyo <cha.git@mailo.fr>
2025-01-26 16:33:31 +0100

    plus

--- game.py
+++ game.py
@@ -10,9 +10,6 @@ class Game:
         self.current_turn = Color.BLUE
         self.agent1 = Color.BLUE
         self.agent2 = Color.RED
-        # self.retreat_activated = False
-        # self.retreat_position = None
-
         self.network = Network()
         self.agent_red = Agent(
             color=Color.RED,
@@ -32,16 +29,11 @@ class Game:
         """
         if selected_paw.color != self.current_turn:
             return f"It's not {selected_paw.color}'s turn."
-        # if self.retreat_activated and not self.board.valid_retreat_move(selected_paw, destination, self.retreat_position):
-        #     return f"This move is not valid during retreat."
         try:
-            # self.retreat_activated = False
             possible_moves = self.board.possible_movements(selected_paw)
             if destination not in possible_moves:
               return f"Invalid move. {destination} is not a valid destination."
             self.board.move_paw(selected_paw, destination)
-            #   self.retreat_position  = destination
-            #   self.retreat_activated = True
         except ValueError as e:
             return str(e)
         if self.board.check_win(destination) != -1:
