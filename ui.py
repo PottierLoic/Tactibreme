56bd997aa03ba384cbad521d223f1abe64906126
Lucccyo <cha.git@mailo.fr>
2025-02-06 14:58:22 +0100

    adapting reward for 2 agents

--- ui.py
+++ ui.py
@@ -6,13 +6,16 @@ def plot_rewards(game):
     plt.ion()
     fig, ax = plt.subplots()
 
-    rewards_agent = []
+    rewards_agent1 = []
+    rewards_agent2 = []
 
     while True:
-        rewards_agent = [entry[2] for entry in game.agent.memory]
+        rewards_agent1 = [entry[2] for entry in game.agent1.memory]
+        rewards_agent2 = [entry[2] for entry in game.agent2.memory]
 
         ax.clear()
-        ax.plot(rewards_agent, label='Agent 1 Rewards', color='blue')
+        ax.plot(rewards_agent1, label='Agent 1 Rewards', color='blue')
+        ax.plot(rewards_agent2, label='Agent 2 Rewards', color='red')
         ax.set_title('AI rewards over episodes')
         ax.set_xlabel('Episode')
         ax.set_ylabel('Reward')
