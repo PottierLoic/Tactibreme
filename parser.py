513686c70cacd1f6cc737e493e81ad35a6c40959
Louise HORTER <louisehorter@gmail.com>
2025-02-26 14:15:57 +0100

    feat(parsing): Add win rate, movement by paw rate, win rate by position, average number of moves statistics

---
+++ parser.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+def calculate_win_rate(csv_file):
+    
+    file = pd.read_csv(csv_file)
+    
+    file['win'] = file['win'].astype(bool)
+    
+    win_rate = file.groupby('agent')['win'].mean() * 100
+    
+    for agent, value in win_rate.items():
+        print(f"Win rate by agent :\nAgent {agent}: {value}%\n")
+
+    return
+
+def calculate_win_rate_pos(csv_file):
+    
+    file = pd.read_csv(csv_file)
+    
+    file['win'] = file['win'].astype(bool)
+    
+    position = file.groupby(['dest_x', 'dest_y'])['win'].mean() * 100
+    
+    for (x,y), value in position.items():
+        print(f"Win rate by position :\nPosition {x}, {y}: {value}%\n")
+
+    return
+
+def calculate_move_paw_rate(csv_file):
+    
+    file = pd.read_csv(csv_file)
+    
+    paw_rate = file.groupby('color')['id_paw'].value_counts(normalize=True) * 100
+
+    for id, value in paw_rate.items():
+        print(f"Movement rate by paw :\n{id}: {value}%")
+    
+    return
+ 
+def average_number_moves(csv_file):
+
+    file = pd.read_csv(csv_file)
+
+    list_games = []
+    number_moves = 0
+
+    for id in file['win']:
+        number_moves = number_moves + 1
+        if id == 1:
+            list_games.append(number_moves)
+            number_moves = 0
+    print("Average number of moves per game :", sum(list_games)/len(list_games))
+
+    return 
+
+if __name__ == "__main__":
+    csv_path = "train_rec_3_model1.csv"
+    calculate_win_rate(csv_path)
+    calculate_move_paw_rate(csv_path)
+    calculate_win_rate_pos(csv_path)
+    average_number_moves(csv_path)
